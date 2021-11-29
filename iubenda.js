a304d396b3b29a844747497a4e0cd3d45d2bd154
Mattia Larentis <larentis@spaziodati.eu>
2021-11-30 00:08:19 +0100

    Updates

---
+++ iubenda.js
@@ -0,0 +1,16 @@
+var _iub = _iub || []
+_iub.csConfiguration = {
+  priorConsent: false,
+  reloadOnConsent: true,
+  lang: "it",
+  siteId: 2419628,
+  cookiePolicyId: 42365225,
+  banner: {
+    content:
+      "Noi e terze parti selezionate utilizziamo cookie o tecnologie simili per finalità tecniche e, con il tuo consenso, anche per altre finalità come specificato nella cookie policy. Puoi acconsentire all’utilizzo di tali tecnologie chiudendo questa informativa.",
+    acceptButtonDisplay: true,
+    customizeButtonDisplay: false,
+    closeButtonDisplay: true,
+    position: "float-top-center",
+  },
+}
