a30f3e13fd3ad44a7a9a1fe02e962538c32f8b61
Mattia Larentis <larentis@spaziodati.eu>
2021-12-12 11:32:13 +0100

    Updates

---
+++ component---src-pages-404-js-9a51f5463479f13afbe7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdudexpress=self.webpackChunkdudexpress||[]).push([[883],{9616:function(e,t,n){n.r(t);var l=n(7294),a=n(5414),u=n(5117);t.default=function(e){var t=e.data,n=e.location,s=t.site.siteMetadata.title;return l.createElement(u.Z,{location:n,title:s},l.createElement(a.q,null,l.createElement("title",null,"404: Not Found")),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9a51f5463479f13afbe7.js.map
