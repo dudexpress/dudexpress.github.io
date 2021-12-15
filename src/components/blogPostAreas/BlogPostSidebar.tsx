14ecdc36d170fa7e7f9982c864b34195cc2787ab
Mattia Larentis <larentis@spaziodati.eu>
2021-12-15 20:19:29 +0100

    add socials to the sidebar

--- src/components/blogPostAreas/BlogPostSidebar.tsx
+++ src/components/blogPostAreas/BlogPostSidebar.tsx
@@ -5,6 +5,7 @@ import SidebarValues from "../sidebar/SidebarVotes"
 import Mechanisms from "../sidebar/Mechanisms"
 import * as style from "./BlogPostSidebar.module.scss"
 import Sleeves from "../sidebar/Sleeves"
+import Socials from "../sidebar/Socials"
 
 export default class BlogPostSidebar extends React.PureComponent<Frontmatter> {
   private renderSleeves(): React.ReactNode {
@@ -29,6 +30,9 @@ export default class BlogPostSidebar extends React.PureComponent<Frontmatter> {
           <SidebarValues values={this.props.sidebar_votes} />
         </div>
         {this.renderSleeves()}
+        <div className="mt-5">
+          <Socials />
+        </div>
       </div>
     )
   }
---
+++ src/components/sidebar/Socials.tsx
@@ -0,0 +1,25 @@
+import { OutboundLink } from "gatsby-plugin-google-gtag"
+import React from "react"
+
+export default class Socials extends React.PureComponent {
+  public render(): React.ReactNode {
+    return (
+      <>
+        <h5>Sempre aggiornato</h5>
+        <OutboundLink
+          href="https://www.instagram.com/dudexpress.review/"
+          target="_blank"
+        >
+          <img width={40} src={`../../socials/instagram.svg`} />
+        </OutboundLink>
+        <OutboundLink
+          href="https://www.facebook.com/dudexpress.review"
+          target="_blank"
+          className="ms-2"
+        >
+          <img width={40} src={`../../socials/facebook.svg`} />
+        </OutboundLink>
+      </>
+    )
+  }
+}
