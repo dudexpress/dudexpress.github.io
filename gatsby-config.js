4678bd0b87f57c21ec8b75e856db5139c457ccb6
Mattia Larentis <larentis@spaziodati.eu>
2022-05-30 20:35:52 +0200

    last friday

--- gatsby-config.js
+++ gatsby-config.js
@@ -238,7 +238,7 @@ module.exports = {
               }
             `,
             output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
+            title: "dudexpress",
           },
         ],
       },
