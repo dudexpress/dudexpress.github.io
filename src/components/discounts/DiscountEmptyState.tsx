026bcc1ff4f8c999c23129d1f224f21dbb3fa4a4
Mattia Larentis <larentis@spaziodati.eu>
2023-02-06 20:06:25 +0100

    add weega empty state, change its logo and color

--- src/components/discounts/DiscountEmptyState.tsx
+++ src/components/discounts/DiscountEmptyState.tsx
@@ -1,9 +1,28 @@
+import classNames from "classnames"
 import React from "react"
 
+import * as style from "./DiscountEmptyState.module.scss"
+
 export const DiscountEmptyState = () => {
+  const className = classNames(
+    style.emptyStateWeega,
+    "mt-5 bg-light p-5 d-flex justify-content-center"
+  )
   return (
     <div>
       <div>Non siamo riusciti a trovare quello che stai cercando :(</div>
+      <div className={className}>
+        <div className="w-50">
+          <img src="../../logo/weega-big.png" width="50%" />
+          <div className="mt-3">
+            Non trovi il gioco che desideri? <br />
+            Fa' la tua proposta su{" "}
+            <a href="https://weega.it/?partners=Dudexpress" target="_blank">
+              Weega!
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
