69a142a1ae568822eedfbc44b052ba4ceec13234
Mattia Larentis <larentis@spaziodati.eu>
2023-03-05 19:42:42 +0100

    first idea of dudefunding

--- src/components/boxes/BaseBox.tsx
+++ src/components/boxes/BaseBox.tsx
@@ -1,3 +1,4 @@
+import classnames from "classnames"
 import React from "react"
 import Card from "react-bootstrap/Card"
 import * as style from "./BaseBox.module.scss"
@@ -5,20 +6,43 @@ import * as style from "./BaseBox.module.scss"
 interface BaseBoxProps {
   title: string
   footer?: React.ReactNode
+  small?: boolean
+  noHeight?: boolean
+  noFooter?: boolean
 }
 
-export default class BaseBox extends React.PureComponent<BaseBoxProps> {
+export default class BaseBox extends React.PureComponent<
+  React.PropsWithChildren<BaseBoxProps>
+> {
   public render(): React.ReactNode {
     return (
-      <Card className={style.baseBox}>
+      <Card
+        className={classnames(style.baseBox, {
+          [style.small]: this.props.small,
+          "h-auto": this.props.noHeight,
+        })}
+      >
         <Card.Body className={style.cardBody}>
-          <Card.Title className={style.cardTitle}>
+          <Card.Title
+            className={classnames(style.cardTitle, {
+              [style.cardTitleSmall]: this.props.small,
+            })}
+          >
             {this.props.title}
           </Card.Title>
-          <Card.Text className={style.cardText}>
+          <Card.Text
+            className={classnames(style.cardText, {
+              [style.cardTextSmall]: this.props.small,
+              [style.cardTextNoHeight]: this.props.noHeight,
+            })}
+          >
             {this.props.children}
           </Card.Text>
-          <Card.Footer className={style.cardFooter}>
+          <Card.Footer
+            className={classnames(style.cardFooter, {
+              [style.cardFooterSmall]: this.props.small,
+            })}
+          >
             {this.props.footer}
           </Card.Footer>
         </Card.Body>
---
+++ src/components/sections/FundingBit.tsx
@@ -0,0 +1,86 @@
+import React from "react"
+
+import BaseSection from "./BaseSection"
+import HypeBox from "../boxes/HypeBox"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import DurationBox from "../boxes/DurationBox"
+import WeightBox from "../boxes/WeightBox"
+import PlayerCountBox from "../boxes/PlayerCountBox"
+import { PostWeigth } from "../../types"
+import DeadlineBox from "../boxes/DeadlineBox"
+import PriceBox from "../boxes/PriceBox"
+import Mechanisms from "../sidebar/Mechanisms"
+import BaseBox from "../boxes/BaseBox"
+
+export interface AdvisorBitProps {
+  title: string
+  hype: number
+  player_count: number
+  player_count_official: string
+  weight: PostWeigth
+  playing_time: string
+  playing_time_official: string
+  deadline: string
+  delivery: string
+  price: string
+  otherPrice: string
+  mechanism: string[]
+}
+
+export const FundingBit = (props: React.PropsWithChildren<AdvisorBitProps>) => {
+  function renderAfterTitle(): JSX.Element {
+    return (
+      <Row className="welcome-boxes">
+        <Col md={6} xl={4} className="p-0">
+          <HypeBox value={props.hype} small />
+        </Col>
+        <Col md={6} xl={4} className="p-0">
+          <DurationBox
+            value={props.playing_time}
+            officialValue={props.playing_time_official}
+            small
+          />
+        </Col>
+        <Col md={6} xl={4} className="p-0">
+          <WeightBox value={props.weight} small />
+        </Col>
+        <Col md={6} xl={4} className="p-0">
+          <PlayerCountBox
+            value={props.player_count}
+            officialValue={props.player_count_official}
+            small
+          />
+        </Col>
+        <Col md={6} xl={4} className="p-0">
+          <DeadlineBox
+            value={props.deadline}
+            deliverylValue={props.delivery}
+            small
+          />
+        </Col>
+        <Col md={6} xl={4} className="p-0">
+          <PriceBox value={props.price} otherValue={props.otherPrice} small />
+        </Col>
+
+        <Col md={12} xl={12} className="p-0">
+          <BaseBox title="Meccaniche" small noHeight>
+            <Mechanisms values={props.mechanism} small />
+          </BaseBox>
+        </Col>
+      </Row>
+    )
+  }
+
+  return (
+    <BaseSection
+      title={props.title}
+      trait="pink"
+      className="mb-4"
+      renderAfterTitle={renderAfterTitle}
+    >
+      <p className="mt-4">{props.children}</p>
+      {/* <PostWriter writerName={writer} asCard={true} inAdvisor={true} /> */}
+    </BaseSection>
+  )
+}
