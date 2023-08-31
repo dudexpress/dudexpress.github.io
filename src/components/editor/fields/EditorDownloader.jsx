733ed80fa01b582bf27d516b59d8ce97de433169
Fora00 <francesco.elia.foradori@gmail.com>
2023-08-31 11:18:10 +0200

    tentative dudeEditor fix

--- src/components/editor/fields/EditorDownloader.jsx
+++ src/components/editor/fields/EditorDownloader.jsx
@@ -112,6 +112,11 @@ sidebar_votes:
       let html = converter.makeHtml(md)
       html = html.replace(/\*\*([\w\s:_?]+)\*\*/gm, "<strong>$1</strong>")
       html = html.replace(/\*([\w\s:_?]+)\*/gm, "<em>$1</em>")
+      html = html.replace(/\_\_([\w\s:_?]+)\_\_/gm, "<u>$1</u>")
+      html = html.replace(/\~\~([\w\s:_?]+)\~\~/gm, "<s>$1</s>")
+      html = html.replace(/(https:\/\/dudexpress\.it)/gm, "")
+      html = html.replace(/(&lt;)/gm, "<")
+      html = html.replace(/(&gt;)/gm, ">")
       html = html.replace(/<p>/g, "")
       html = html.replace(/<\/p>/g, "<br />")
 
--- src/components/editor/fields/EditorWYSIWYG.jsx
+++ src/components/editor/fields/EditorWYSIWYG.jsx
@@ -1,7 +1,10 @@
-import React from "react"
-import Form from "react-bootstrap/Form"
-import { Editor } from "react-draft-wysiwyg"
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
+
+import { EditorState, Modifier } from "draft-js"
+import React, { Component } from "react"
+
+import { Editor } from "react-draft-wysiwyg"
+import Form from "react-bootstrap/Form"
 
 export const EditorWYSIWYG = ({ name, value, setValue, text }) => {
   return (
@@ -16,14 +19,43 @@ export const EditorWYSIWYG = ({ name, value, setValue, text }) => {
         toolbar={{
           options: ["inline", "list"],
           inline: {
-            options: ["bold", "italic", "underline"],
+            options: ["bold", "italic", "underline", "strikethrough"],
           },
           list: {
             options: ["unordered", "ordered"],
           },
         }}
+        toolbarCustomButtons={[<CustomLink />]}
       />
       <Form.Text className="text-muted">{text}</Form.Text>
     </Form.Group>
   )
+}
+
+class CustomLink extends Component {
+  addStar = () => {
+    const { editorState, onChange } = this.props
+    const contentState = Modifier.replaceText(
+      editorState.getCurrentContent(),
+      editorState.getSelection(),
+      '<Link to="link">testo</Link>',
+      editorState.getCurrentInlineStyle()
+    )
+    onChange(EditorState.push(editorState, contentState, "insert-characters"))
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          marginLeft: "10px",
+          padding: "0",
+          fontSize: "15px",
+        }}
+        onClick={this.addStar}
+      >
+        🔗
+      </div>
+    )
+  }
 }
