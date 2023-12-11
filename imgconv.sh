03da6c7beb2d6d25e830b9a6085f2851cac6d540
Mattia Larentis <larentis@spaziodati.eu>
2023-12-11 12:10:31 +0100

    fix imgconv

--- imgconv.sh
+++ imgconv.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-find ./content/blog -type f -iname "*.jpeg" -exec mogrify -verbose -format jpeg -layers Dispose -resize 1500\>x1500\> -quality 100% {} +
-find ./content/blog -type f -iname "*.jpg" -exec mogrify -verbose -format jpg -layers Dispose -resize 1500\>x1500\> -quality 100% {} +
-find ./content/blog -type f -iname "*.png" -exec mogrify -verbose -format png -alpha on -layers Dispose -resize 1500\>x1500\> {} +
+minimumWidth=1500
+minimumHeight=1500
+
+for f in $( find ./content/blog -type f -iname "*.jpg" )
+do
+    imageWidth=$(identify -format "%w" "$f")
+    imageHeight=$(identify -format "%h" "$f")
+
+    if [ "$imageWidth" -gt "$minimumWidth" ] || [ "$imageHeight" -gt "$minimumHeight" ]; then
+        mogrify -verbose -layers Dispose -resize ''"$minimumWidth"x"$minimumHeight"'' -quality 100% {} + $f
+    fi
+done
+
+for f in $( find ./content/blog -type f -iname "*.png" )
+do
+    imageWidth=$(identify -format "%w" "$f")
+    imageHeight=$(identify -format "%h" "$f")
+
+    if [ "$imageWidth" -gt "$minimumWidth" ] || [ "$imageHeight" -gt "$minimumHeight" ]; then
+        mogrify -verbose -layers Dispose --alpha on -resize ''"$minimumWidth"x"$minimumHeight"'' -quality 100% {} + $f
+    fi
+done
