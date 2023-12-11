1b2507588a219fe4ec1f7851d42be50687b4301b
Mattia Larentis <larentis@spaziodati.eu>
2023-12-11 09:04:06 +0100

    add sky team

--- imgconv.sh
+++ imgconv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find ./content/blog -type f -iname "*.jpeg" -exec mogrify -verbose -format jpeg -layers Dispose -resize 1500\>x1500\> -quality 75% {} +
-find ./content/blog -type f -iname "*.jpg" -exec mogrify -verbose -format jpg -layers Dispose -resize 1500\>x1500\> -quality 75% {} +
+find ./content/blog -type f -iname "*.jpeg" -exec mogrify -verbose -format jpeg -layers Dispose -resize 1500\>x1500\> -quality 100% {} +
+find ./content/blog -type f -iname "*.jpg" -exec mogrify -verbose -format jpg -layers Dispose -resize 1500\>x1500\> -quality 100% {} +
 find ./content/blog -type f -iname "*.png" -exec mogrify -verbose -format png -alpha on -layers Dispose -resize 1500\>x1500\> {} +
